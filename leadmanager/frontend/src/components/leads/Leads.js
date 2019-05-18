b0a188945fd7913a81652d379b2a4bfb5b7eb8c7
koad7 <kodjo.adanledji@weforum.org>
2019-05-18 20:12:00 +0200

    Implementing React

---
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export class Leads extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Least List</h1>
+      </div>
+    )
+  }
+}
+export default Leads
